9a6490cf62e2754945701770b3eddeb823584123
Bernhard <wst.lordfish@gmail.com>
2016-03-18 20:18:45 +0100

    Added files via upload

---
+++ files/js/lang.js
@@ -0,0 +1,71 @@
+(function() {
+	if (localStorage['lang'] == null) {
+		var lang = window.navigator.userLanguage || window.navigator.language;
+		lang = lang.substring(0,2);
+		localStorage['lang'] = lang;
+	}
+
+	var options = {
+		debug: false,
+		getAsync: true,
+		ns: 'general',
+		lng: localStorage['lang'],
+		fallbackLng: 'en',
+		resGetPath: '../files/locales/__lng__/__ns__.json',
+		useDataAttrOptions: true,
+		lngWhitelist: [ 'en', 'de' ]
+	};
+
+	$.i18n.init(options, function() {
+		var namespace = location.pathname.match(/\/(\a{1})\/(?:index.html)?$/)[1];
+		if (options.debug) console.log('i18next initialization complete.  loading namespace:'+namespace);
+		$.i18n.loadNamespace(namespace, function() {
+			if (options.debug) console.log('i18next is ready.');
+			if (options.debug) console.log('loading shared');
+			$(document).trigger('loadShared');
+
+			if (options.debug) console.log('loading mapdata');
+			$(document).trigger('loadMapdata');
+
+			if (options.debug) console.log('loading custom');
+			$(document).trigger('loadCustom');
+
+			if (options.debug) console.log('performing language substitutions')
+			$(document).i18n();
+
+			if (options.debug) console.log('map loaded and ready')
+		});
+	});
+
+	window.changeLang = function(lang) {
+		localStorage['lang'] = lang;
+		$.i18n.setLng(lang, function() { $(document).i18n(); });
+	};
+
+	// var languageOptions = [
+		// {text: "English",value: "en",selected: (localStorage['lang'] == "en" ? true : false), description: " ",imageSrc: "../files/img/flags/en.png"},
+		// {text: "Deutsch",value: "de",selected: (localStorage['lang'] == "de" ? true : false),description: " ",imageSrc: "../files/img/flags/de.png"},
+		// {text: "Slovak",value: "sk",selected: (localStorage['lang'] == "sk" ? true : false),description: " ",imageSrc: "../files/img/flags/sk.png"},
+		// {text: "Polski",value: "pl",selected: (localStorage['lang'] == "pl" ? true : false),description: " ",imageSrc: "../files/img/flags/pl.png"},
+		// {text: "Spanish",value: "es",selected: (localStorage['lang'] == "es" ? true : false),description: " ",imageSrc: "../files/img/flags/es.png"},
+		// {text: "Portuguese",value: "br",selected: (localStorage['lang'] == "br" ? true : false),description: " ",imageSrc: "../files/img/flags/br.png"},
+		// {text: "Dutch",value: "nl",selected: (localStorage['lang'] == "nl" ? true : false),description: " ",imageSrc: "../files/img/flags/nl.png"},
+		// {text: "Italian",value: "it",selected: (localStorage['lang'] == "it" ? true : false),description: " ",imageSrc: "../files/img/flags/it.png"},
+		// {text: "Korean ",value: "kr",selected: (localStorage['lang'] == "kr" ? true : false),description: " ",imageSrc: "../files/img/flags/kr.png"}
+	// ];
+
+	// $('#lang-switcher').ddslick({
+		// data: languageOptions,
+		// width: 150,
+		// onSelected: function(obj){
+			// changeLang(obj.selectedData.value);
+		// }
+	// });
+
+	//fix bug where sidebar scrollbar doesn't appear when the language drop-down opens
+	$('.dd-selected').on('click', function() {
+		setTimeout(function() {
+				$("#sidebar").getNiceScroll().resize();
+		}, 500);
+	});
+})();
---
+++ files/js/shared.js
@@ -0,0 +1,149 @@
+$(document).on("loadShared", function () {
+	window.createMarker = function (coord, icon, label, popup, dataKey) {
+		var mapKey = 'markers-' + map_path + '-hidden';
+		var marker = L.marker(coord, setMarker(icon)).bindLabel(label).bindPopup(popup);
+
+		marker.on('contextmenu', function (e) {
+			toggleOpacity(e, mapKey);
+			updatePills(e, dataKey);
+		});
+
+		if (isMarkerInvisible(mapKey, marker.getLatLng().lat, marker.getLatLng().lng)) {
+			marker.setOpacity(invisibleMarkerOpacity);
+			if(!markerCount[dataKey]) markerCount[dataKey] = 0;
+		} else {
+			markerCount[dataKey] = (markerCount[dataKey] + 1) || 1;
+		}
+
+		return marker;
+	};
+
+	window.setMarker = function (icon, tooltip) {
+		return {icon : icon, riseOnHover : true};
+	};
+
+	window.getLatLngKey = function (lat, lng) {
+		return lat + ';' + lng;
+	};
+
+	window.isMarkerInvisible = function (mapPath, lat, lng) {
+		return invisibleMarkers[mapPath].indexOf(getLatLngKey(lat, lng)) > -1;
+	};
+
+	window.toggleOpacity = function (event, mapPath) {
+		var key = getLatLngKey(event.latlng.lat, event.latlng.lng);
+
+		if (event.target && event.target.options.opacity === 1.0) {
+			event.target.setOpacity(invisibleMarkerOpacity);
+			invisibleMarkers[mapPath].push(key);
+		} else {
+			event.target.setOpacity(1.0);
+			invisibleMarkers[mapPath].splice(invisibleMarkers[mapPath].indexOf(key), 1);
+		}
+
+		localStorage[mapPath] = JSON.stringify(invisibleMarkers[mapPath]);
+	};
+
+	window.updatePills = function(event, dataKey) {
+		if (event.target && event.target.options.opacity === 1.0) {
+			markerCount[dataKey] = (markerCount[dataKey] + 1) || 1;
+		} else {
+			markerCount[dataKey] = (markerCount[dataKey] - 1) || 0;
+		}
+		$('ul.key:not(.controls) > li:not(.none) > i.'+dataKey+' ~ :last').text(markerCount[dataKey]);
+	};
+
+	window.resetInvisibleMarkers = function() {
+		var mapKey = 'markers-' + map_path + '-hidden';
+		invisibleMarkers[mapKey] = [];
+		localStorage[mapKey] = JSON.stringify(invisibleMarkers[mapKey]);
+		location.reload();
+	};
+
+	window.icons = {};
+	window.markers = {};
+	window.invisibleMarkers = {};
+	window.markerCount = {};
+	window.notes = {};
+
+	var icons = window.icons;
+	var markers = window.markers;
+	var invisibleMarkerOpacity = 0.25;
+
+	window.processData = function (data) {
+		var mapKey = 'markers-' + map_path + '-hidden';
+
+		if(!localStorage[mapKey]) {
+			localStorage[mapKey] = JSON.stringify([]);
+		}
+		invisibleMarkers[mapKey] = JSON.parse(localStorage[mapKey]);
+
+		var notesKey = 'notes'+map_path;
+		if(!localStorage[notesKey]) {
+			localStorage[notesKey] = JSON.stringify([]);
+		}
+		notes[map_path] = JSON.parse(localStorage[notesKey]);
+
+		Object.keys(data).forEach(function (dataKey) {
+			var items = data[dataKey];
+			var groupItems = [];
+			items.forEach(function (item) {
+				if (item.popupTitle == null) {
+					item.popupTitle = item.label;
+				}
+				item.coords.forEach(function (coord) {
+					groupItems.push(createMarker(coord, icons[dataKey], item.label, '<h1>' + item.popupTitle + '</h1>' + item.popup, dataKey));
+				});
+			});
+			markers[dataKey] = L.layerGroup(groupItems);
+		});
+	};
+
+	icons.note_marker = L.icon({
+		iconUrl  : '../files/img/icons/note_marker.png',
+		iconSize : [32, 48]
+	});
+
+	icons.weapons = L.icon({
+		iconUrl  : '../files/img/icons/weapons.png',
+		iconSize : [32, 48]
+	});
+	icons.achievement = L.icon({
+		iconUrl  : '../files/img/icons/achievement.png',
+		iconSize : [32, 48]
+	});
+
+	icons.kleidung = L.icon({
+		iconUrl  : '../files/img/icons/kleidung.png',
+		iconSize : [32, 48]
+	});
+
+	icons.challenge = L.icon({
+		iconUrl  : '../files/img/icons/challenge.png',
+		iconSize : [32, 48]
+	});
+
+	icons.info = L.icon({
+		iconUrl  : '../files/img/icons/info.png',
+		iconSize : [32, 48]
+	});
+
+	icons.feats = L.icon({
+		iconUrl  : '../files/img/icons/feats.png',
+		iconSize : [32, 48]
+	});
+
+	icons.assassination = L.icon({
+		iconUrl  : '../files/img/icons/assassination.png',
+		iconSize : [32, 48]
+	});
+
+
+
+
+	icons.item = L.icon({
+		iconUrl  : '../files/img/icons/item.png',
+		iconSize : [32, 48]
+	});
+
+});
