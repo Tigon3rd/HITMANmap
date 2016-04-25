4916698f2b6b4163a5cd394242f94451fcd35eae
lordfiSh <wst.lordfish@gmail.com>
2016-04-25 23:28:01 +0200

    prep. for 2. dlc

---
+++ files/js/mapdata-z.js
@@ -0,0 +1,42 @@
+$(document).on("loadMapdata", function () {
+	window.map_path   = 'z';
+	window.map_sWest  = L.latLng(-100, -200);
+	window.map_nEast  = L.latLng(500, 200);
+	window.map_center = [-70,-20];
+	window.map_mZoom  = 6;
+
+	processData({
+
+	weapons: [],
+
+
+	item: [],
+
+  feats: [],
+
+assassinations : [],
+
+
+	challenges : [{
+		coords: [[-50.205,-64.512]],
+		label: $.t("z:challenges.label.delivery") +$.t("discovery.label"),
+		popup: $.t("z:challenges.desc.delivery")
+	}],
+
+
+	achievements : [],
+
+	disguises: []
+
+
+});
+	window.allLayers = [
+		markers.weapons,
+		markers.feats,
+		markers.achievements,
+		markers.challenges,
+		markers.assassinations,
+		markers.item,
+		markers.disguises
+		];
+});
